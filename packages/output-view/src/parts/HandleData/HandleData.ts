25f0bf7f4b90de310aed163964f99f2080c45d7d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-11 16:54:45 +0200

    feature: add handleData command (#4)

---
+++ packages/output-view/src/parts/HandleData/HandleData.ts
@@ -0,0 +1,8 @@
+import type { OutputState } from '../OutputState/OutputState.ts'
+
+export const handleData = async (state: OutputState): Promise<OutputState> => {
+  // TODO
+  return {
+    ...state,
+  }
+}
---
+++ packages/output-view/src/parts/OpenFindWidget/OpenFindWidget.ts
@@ -0,0 +1,8 @@
+import type { OutputState } from '../OutputState/OutputState.ts'
+
+export const openFindWidget = async (state: OutputState): Promise<OutputState> => {
+  // TODO
+  return {
+    ...state,
+  }
+}
