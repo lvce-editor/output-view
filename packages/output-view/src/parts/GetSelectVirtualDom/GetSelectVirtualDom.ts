6b589ae4b7323966a9faf4110b0af00997a0a5f3
Le Vivilet <levivlet@pm.me>
2025-08-13 00:01:05 +0200

    feature: register change event listener

--- packages/output-view/src/parts/GetSelectVirtualDom/GetSelectVirtualDom.ts
+++ packages/output-view/src/parts/GetSelectVirtualDom/GetSelectVirtualDom.ts
@@ -2,6 +2,8 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { Option } from '../Option/Option.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as InputName from '../InputName/InputName.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getOptionVirtualDom } from '../GetOptionVirtualDom/GetOptionVirtualDom.ts'
 
 export const getSelectVirtualDom = (options: readonly Option[]): readonly VirtualDomNode[] => {
@@ -10,7 +12,8 @@ export const getSelectVirtualDom = (options: readonly Option[]): readonly Virtua
       type: VirtualDomElements.Select,
       className: ClassNames.Select,
       childCount: options.length,
-      name: 'Output',
+      name: InputName.Output,
+      onChange: DomEventListenerFunctions.HandleSelect,
     },
     ...options.flatMap(getOptionVirtualDom),
   ]
