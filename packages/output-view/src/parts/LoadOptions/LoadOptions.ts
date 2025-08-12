5220b0e42cfe255ead1c43f0405d10bfe50d8c87
Le Vivilet <levivlet@pm.me>
2025-08-13 00:16:59 +0200

    feature: update option ids

--- packages/output-view/src/parts/LoadOptions/LoadOptions.ts
+++ packages/output-view/src/parts/LoadOptions/LoadOptions.ts
@@ -1,13 +1,14 @@
 import type { Option } from '../Option/Option.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const loadOptions = async (platform: number): Promise<readonly Option[]> => {
   return [
     {
-      id: 'Main',
+      id: InputName.MainProcess,
       uri: 'file:///tmp/log-main-process.txt',
     },
     {
-      id: 'shared',
+      id: InputName.SharedProcess,
       uri: 'File:///tmp/log-shared-process.txt',
     },
   ]
