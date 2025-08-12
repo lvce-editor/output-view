740619b53c57f640756d4ec108cb2fbb8da98fd3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-12 18:52:17 +0200

    feature: add output dom function (#8)

--- packages/output-view/src/parts/RenderItems/RenderItems.ts
+++ packages/output-view/src/parts/RenderItems/RenderItems.ts
@@ -1,9 +1,9 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import type { OutputState } from '../OutputState/OutputState.ts'
 import type { ViewletCommand } from '../ViewletCommand/ViewletCommand.ts'
+import { getOutputVirtualDom } from '../GetOutputVirtualDom/GetOutputVirtualDom.ts'
 
 export const renderItems = (oldState: OutputState, newState: OutputState): ViewletCommand => {
-  // TODO
-  const dom: readonly VirtualDomNode[] = []
+  const dom: readonly VirtualDomNode[] = getOutputVirtualDom()
   return ['Viewlet.setDom2', dom]
 }
