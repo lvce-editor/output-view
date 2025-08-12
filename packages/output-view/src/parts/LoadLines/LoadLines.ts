01c021a940710e5a652a64f03db44f4e2c25215a
Le Vivilet <levivlet@pm.me>
2025-08-12 19:22:22 +0200

    feature: load either content or return error

--- packages/output-view/src/parts/LoadLines/LoadLines.ts
+++ packages/output-view/src/parts/LoadLines/LoadLines.ts
@@ -1,8 +1,19 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { LoadLinesResult } from '../LoadLinesResult/LoadLinesResult.ts'
 
-export const loadLines = async (uri: string): Promise<readonly string[]> => {
-  // TODO use log stream, updating the output when the file is changed
-  const content = await RendererWorker.invoke('FileSystem.readFile', uri)
-  const lines = content.split('\n')
-  return lines
+export const loadLines = async (uri: string): Promise<LoadLinesResult> => {
+  try {
+    // TODO use log stream, updating the output when the file is changed
+    const content = await RendererWorker.invoke('FileSystem.readFile', uri)
+    const lines = content.split('\n')
+    return {
+      error: '',
+      lines,
+    }
+  } catch (error) {
+    return {
+      error: `${error}`,
+      lines: [],
+    }
+  }
 }
