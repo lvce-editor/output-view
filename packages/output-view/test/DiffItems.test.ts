a54e483e3095ceab9d78c1819256b3d30ea8cf22
Le Vivilet <levivlet@pm.me>
2025-08-12 18:57:08 +0200

    test

--- packages/output-view/test/DiffItems.test.ts
+++ packages/output-view/test/DiffItems.test.ts
@@ -5,12 +5,12 @@ import { isEqual } from '../src/parts/DiffItems/DiffItems.ts'
 
 test('isEqual returns true when problems arrays are the same reference', () => {
   const oldState: OutputState = { ...createDefaultState() }
-  const newState: OutputState = { ...createDefaultState() }
+  const newState: OutputState = { ...oldState }
   expect(isEqual(oldState, newState)).toBe(true)
 })
 
 test.skip('isEqual returns false when problems arrays are different', () => {
   const oldState: OutputState = { ...createDefaultState() }
-  const newState: OutputState = { ...createDefaultState() }
+  const newState: OutputState = { ...oldState }
   expect(isEqual(oldState, newState)).toBe(false)
 })
