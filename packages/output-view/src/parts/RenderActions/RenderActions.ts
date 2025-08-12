6b7d3f0b7fcce1d467749639528481939370a568
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-13 00:09:54 +0200

    feature: render state options (#47)

--- packages/output-view/src/parts/RenderActions/RenderActions.ts
+++ packages/output-view/src/parts/RenderActions/RenderActions.ts
@@ -1,13 +1,9 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import type { OutputState } from '../OutputState/OutputState.ts'
 import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
 
-export const renderActions = (uid: number): readonly VirtualDomNode[] => {
-  const actions: readonly any[] = [
-    {
-      id: 'test',
-      name: 'test',
-    },
-  ]
-  const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
+export const renderActions = (state: OutputState): readonly VirtualDomNode[] => {
+  const { options } = state
+  const dom = GetActionsVirtualDom.getActionsVirtualDom(options)
   return dom
 }
