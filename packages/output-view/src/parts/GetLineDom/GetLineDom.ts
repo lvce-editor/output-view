c66b05e417dff3416cba49cd06386dd7bc7cd8fb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-12 19:18:06 +0200

    feature: hoist dom dom (#18)

--- packages/output-view/src/parts/GetLineDom/GetLineDom.ts
+++ packages/output-view/src/parts/GetLineDom/GetLineDom.ts
@@ -1,13 +1,12 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 
+const parentNode: VirtualDomNode = {
+  type: VirtualDomElements.Div,
+  className: 'Line',
+  childCount: 1,
+}
+
 export const getLineDom = (line: string): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.Div,
-      className: 'Line',
-      childCount: 1,
-    },
-    text(line),
-  ]
+  return [parentNode, text(line)]
 }
