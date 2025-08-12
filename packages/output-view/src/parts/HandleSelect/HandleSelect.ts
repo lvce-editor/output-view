bf5245d73936c71fc19cadcbbf07cd42db6d1968
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-13 00:17:31 +0200

    feature: implement handleSelect (#48)

--- packages/output-view/src/parts/HandleSelect/HandleSelect.ts
+++ packages/output-view/src/parts/HandleSelect/HandleSelect.ts
@@ -1,8 +1,18 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
+import { loadLines } from '../LoadLines/LoadLines.ts'
 
-export const handleSelect = async (state: OutputState): Promise<OutputState> => {
-  // TODO update output channel
+export const handleSelect = async (state: OutputState, id: string): Promise<OutputState> => {
+  const { options } = state
+  const matchingOption = options.find((option) => option.id === id)
+  if (!matchingOption) {
+    return state
+  }
+  // TODO potential race condtion when switching output channels fast
+  const { lines, error, code } = await loadLines(matchingOption.uri)
   return {
     ...state,
+    listItems: lines,
+    error,
+    errorCode: code,
   }
 }
