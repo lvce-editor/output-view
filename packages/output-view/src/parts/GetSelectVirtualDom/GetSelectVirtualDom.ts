e9a6a20f92f63d6dd356d37d9a69d7373a3c39d5
Le Vivilet <levivlet@pm.me>
2025-08-13 00:06:32 +0200

    lint

--- packages/output-view/src/parts/GetSelectVirtualDom/GetSelectVirtualDom.ts
+++ packages/output-view/src/parts/GetSelectVirtualDom/GetSelectVirtualDom.ts
@@ -2,9 +2,9 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { Option } from '../Option/Option.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as InputName from '../InputName/InputName.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import { getOptionVirtualDom } from '../GetOptionVirtualDom/GetOptionVirtualDom.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const getSelectVirtualDom = (options: readonly Option[]): readonly VirtualDomNode[] => {
   return [
