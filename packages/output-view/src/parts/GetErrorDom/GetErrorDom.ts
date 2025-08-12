2c4623a141437a72a4559df3005375708f1ab992
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-12 19:22:39 +0200

    feature: add error dom function (#21)

---
+++ packages/output-view/src/parts/GetErrorDom/GetErrorDom.ts
@@ -0,0 +1,14 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+
+export const getErrorDom = (error: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'Error',
+      tabIndex: 0,
+      childCount: 1,
+    },
+    text(error),
+  ]
+}
