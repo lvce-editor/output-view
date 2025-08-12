a283c7abd54f5f42042dffeeb3021a666220deb7
Le Vivilet <levivlet@pm.me>
2025-08-12 19:22:41 +0200

    feature: add getContentDom

---
+++ packages/output-view/src/parts/GetContentDom/GetContentDom.ts
@@ -0,0 +1,16 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker';
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { getLineDom } from '../GetLineDom/GetLineDom.ts'
+
+export const getContentDom = (lines: readonly string[]): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: 'OutputContent',
+      role: 'log',
+      tabIndex: 0,
+      childCount: lines.length,
+    },
+    ...lines.flatMap(getLineDom),
+  ]
+}
