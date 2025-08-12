3045a59a91c0cfc1f98e61a36dc1853b4db6c133
Le Vivilet <levivlet@pm.me>
2025-08-12 23:41:31 +0200

    feature: add basic output actions

--- packages/output-view/src/parts/GetActionsVirtualDom/GetActionsVirtualDom.ts
+++ packages/output-view/src/parts/GetActionsVirtualDom/GetActionsVirtualDom.ts
@@ -1,7 +1,17 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { ViewletAction } from '../ViewletAction/ViewletAction.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+
+const getActionDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      childCount: 1,
+    },
+    text('test action'),
+  ]
+}
 
 export const getActionsVirtualDom = (actions: readonly ViewletAction[]): readonly VirtualDomNode[] => {
   return [
@@ -11,5 +21,6 @@ export const getActionsVirtualDom = (actions: readonly ViewletAction[]): readonl
       role: AriaRoles.ToolBar,
       childCount: actions.length,
     },
+    ...actions.flatMap(getActionDom),
   ]
 }
--- packages/output-view/src/parts/RenderActions/RenderActions.ts
+++ packages/output-view/src/parts/RenderActions/RenderActions.ts
@@ -2,7 +2,12 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
 
 export const renderActions = (uid: number): readonly VirtualDomNode[] => {
-  const actions: readonly any[] = []
+  const actions: readonly any[] = [
+    {
+      id: 'test',
+      name: 'test',
+    },
+  ]
   const dom = GetActionsVirtualDom.getActionsVirtualDom(actions)
   return dom
 }
