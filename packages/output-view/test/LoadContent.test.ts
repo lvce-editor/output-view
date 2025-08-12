e3e676fd72803c537f34a480f73650848e128642
Le Vivilet <levivlet@pm.me>
2025-08-12 19:11:17 +0200

    test

--- packages/output-view/test/LoadContent.test.ts
+++ packages/output-view/test/LoadContent.test.ts
@@ -2,7 +2,18 @@ import { test, expect } from '@jest/globals'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { loadContent } from '../src/parts/LoadContent/LoadContent.ts'
 
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
+import { MockRpc } from '@lvce-editor/rpc'
+
 test('loadContent returns a new state with expected properties', async () => {
+  RendererWorker.set(
+    MockRpc.create({
+      commandMap: {},
+      invoke() {
+        return 'test content'
+      },
+    }),
+  )
   const state = createDefaultState()
   const savedState = {}
   const result = await loadContent(state, savedState)
@@ -10,7 +21,7 @@ test('loadContent returns a new state with expected properties', async () => {
     message: expect.any(String),
     filterValue: '',
     inputSource: expect.any(Number),
-    listItems: expect.any(Array),
+    listItems: ['test content'],
     collapsedUris: [],
   })
 })
