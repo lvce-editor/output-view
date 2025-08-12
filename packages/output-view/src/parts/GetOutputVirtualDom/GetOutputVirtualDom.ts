a4b33833aec530f7f4a27bc4c3a9f6df1bde891c
Le Vivilet <levivlet@pm.me>
2025-08-12 18:52:43 +0200

    feature: add output class

--- packages/output-view/src/parts/GetOutputVirtualDom/GetOutputVirtualDom.ts
+++ packages/output-view/src/parts/GetOutputVirtualDom/GetOutputVirtualDom.ts
@@ -1,10 +1,12 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { mergeClassNames, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 
 export const getOutputVirtualDom = (): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
+      className: mergeClassNames(ClassNames.Viewlet, ClassNames.Output),
       childCount: 1,
     },
     text('Output View'),
