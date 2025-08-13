e4dc57c4723628da159110b322ea8d910ec57fdb
Le Vivilet <levivlet@pm.me>
2025-08-13 11:58:22 +0200

    fix: clear not working

--- packages/output-view/src/parts/GetSelectedItem/GetSelectedItem.ts
+++ packages/output-view/src/parts/GetSelectedItem/GetSelectedItem.ts
@@ -1,6 +1,8 @@
+import * as InputName from '../InputName/InputName.ts'
+
 export const getSelectedItem = (platform: number): string => {
   if (platform === /* Web */ 1) {
     return ''
   }
-  return 'file:///tmp/log-main-process.txt'
+  return InputName.MainProcess
 }
--- packages/output-view/src/parts/LoadContent/LoadContent.ts
+++ packages/output-view/src/parts/LoadContent/LoadContent.ts
@@ -19,9 +19,13 @@ const getSavedCollapsedUris = (savedState: any): readonly string[] => {
 export const loadContent = async (state: OutputState, savedState: any): Promise<OutputState> => {
   const platform = /* Electron */ 4
   const collapsedUris = getSavedCollapsedUris(savedState)
-  const selectedUri = getSelectedItem(platform)
+  const selectedId = getSelectedItem(platform)
   const options = await loadOptions(platform)
-  const { lines, error, code } = await loadLines(selectedUri)
+  const uri = options.find((option) => option.id === selectedId)?.uri
+  if (!uri) {
+    throw new Error('option not found')
+  }
+  const { lines, error, code } = await loadLines(uri)
   const buttons = loadButtons()
   return {
     ...state,
@@ -31,7 +35,7 @@ export const loadContent = async (state: OutputState, savedState: any): Promise<
     inputSource: InputSource.Script,
     listItems: lines,
     options,
-    selectedOption: selectedUri,
+    selectedOption: selectedId,
     buttons,
   }
 }
