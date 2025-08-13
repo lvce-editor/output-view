62d91822b795a011b36ad9f19ae23cc8d29106c5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-13 16:57:08 +0200

    feature: add extension host rpc (#94)

--- packages/output-view/src/parts/Initialize/Initialize.ts
+++ packages/output-view/src/parts/Initialize/Initialize.ts
@@ -1,6 +1,6 @@
-import { initializeExtensionHostWorker } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
+import { initializeExtensionHost } from '../InitializeExtensionHostWorker/InitializeExtensionHostWorker.ts'
 import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 
 export const initialize = async (): Promise<void> => {
-  await Promise.all([initializeFileSystemWorker(), initializeExtensionHostWorker()])
+  await Promise.all([initializeFileSystemWorker(), initializeExtensionHost()])
 }
