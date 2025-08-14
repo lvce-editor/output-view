b6931ff3532d9d0901f474a7d8359629161c152e
Le Vivilet <levivlet@pm.me>
2025-08-14 18:01:37 +0200

    fix: update filteredItems when changing output channel

--- packages/output-view/src/parts/SelectChannel/SelectChannel.ts
+++ packages/output-view/src/parts/SelectChannel/SelectChannel.ts
@@ -1,19 +1,22 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
 import { loadLines } from '../LoadLines/LoadLines.ts'
+import { filterItems } from '../FilterItems/FilterItems.ts'
 
 export const selectChannel = async (state: OutputState, id: string): Promise<OutputState> => {
-  const { options } = state
+  const { options, filterValue } = state
   const matchingOption = options.find((option) => option.id === id)
   if (!matchingOption) {
     return state
   }
   // TODO potential race condtion when switching output channels fast
   const { lines, error, code } = await loadLines(matchingOption.uri)
+  const filteredItems = filterItems(lines, filterValue)
   return {
     ...state,
     error,
     errorCode: code,
     listItems: lines,
+    filteredItems,
     selectedOption: id,
   }
 }
