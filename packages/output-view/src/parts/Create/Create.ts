bbe0de8a789d83c09adc2ed6cd9cc3de8c5cbc7d
Le Vivilet <levivlet@pm.me>
2025-08-14 17:43:49 +0200

    feature: improve error handling

--- packages/output-view/src/parts/Create/Create.ts
+++ packages/output-view/src/parts/Create/Create.ts
@@ -1,8 +1,10 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
+import * as Assert from '../Assert/Assert.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 import * as OutputStates from '../OutputStates/OutputStates.ts'
 
 export const create = (id: number, uri: string, x: number, y: number, width: number, height: number, platform: number, parentId: number): void => {
+  Assert.number(parentId)
   const state: OutputState = {
     uid: id,
     parentId,
