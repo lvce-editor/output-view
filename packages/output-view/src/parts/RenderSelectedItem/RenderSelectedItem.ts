5fe89a87198e7bed8751e2c679bb76690c4e9d1d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 11:13:21 +0200

    feature: render selected value (#112)
    * feature: render selected value
    
    * log

--- packages/output-view/src/parts/RenderSelectedItem/RenderSelectedItem.ts
+++ packages/output-view/src/parts/RenderSelectedItem/RenderSelectedItem.ts
@@ -2,6 +2,7 @@ import type { OutputState } from '../OutputState/OutputState.ts'
 import type { ViewletCommand } from '../ViewletCommand/ViewletCommand.ts'
 
 export const renderSelectedItem = (oldState: OutputState, newState: OutputState): ViewletCommand => {
-  // TODO
-  return ['']
+  const value = newState.selectedOption
+  const name = 'output'
+  return ['Viewlet.setValueByName', name, value]
 }
