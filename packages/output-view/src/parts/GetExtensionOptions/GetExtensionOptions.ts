15447af34d658fefb73ba1485ad560a6eea7b73c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 17:32:40 +0200

    feature: activate by event before querying extensions (#124)

--- packages/output-view/src/parts/GetExtensionOptions/GetExtensionOptions.ts
+++ packages/output-view/src/parts/GetExtensionOptions/GetExtensionOptions.ts
@@ -1,12 +1,19 @@
 import type { Option } from '../Option/Option.ts'
 import * as ExtensionHostWorker from '../ExtensionHostWorker/ExtensionHostWorker.ts'
+import * as Logger from '../Logger/Logger.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const getExtensionOptions = async (): Promise<readonly Option[]> => {
   try {
+    // TODO make api more declarative:
+    // output channels are registered in extension manifest
+    // only downside: it might show channels for extensions that are not active
+    await RendererWorker.activateByEvent('onOutput')
     // @ts-ignore
     const channels = await ExtensionHostWorker.invoke('Output.getEnabledProviders')
     return channels
-  } catch {
+  } catch (error) {
+    Logger.error(error)
     return []
   }
 }
