a69de1b84e322f16071ab0d0eb0f389ed6b5d326
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 16:02:47 +0200

    feature: use input enum (#120)

--- packages/output-view/src/parts/RenderSelectedItem/RenderSelectedItem.ts
+++ packages/output-view/src/parts/RenderSelectedItem/RenderSelectedItem.ts
@@ -1,8 +1,9 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
 import type { ViewletCommand } from '../ViewletCommand/ViewletCommand.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const renderSelectedItem = (oldState: OutputState, newState: OutputState): ViewletCommand => {
   const value = newState.selectedOption
-  const name = 'output'
+  const name = InputName.Output
   return ['Viewlet.setValueByName', newState.uid, name, value]
 }
