f45615f602f59dfbc4ba6386f05518a7ba26d6de
Le Vivilet <levivlet@pm.me>
2025-08-14 09:15:53 +0200

    tsc

--- packages/output-view/src/parts/SetupChangeListener/SetupChangeListener.ts
+++ packages/output-view/src/parts/SetupChangeListener/SetupChangeListener.ts
@@ -2,11 +2,15 @@ import { FileSystemWorker } from '@lvce-editor/rpc-registry'
 import * as WatchCallbacks from '../WatchCallbacks/WatchCallbacks.ts'
 
 export const setupChangeListener = async (uri: string): Promise<void> => {
-  const watchId = Math.random()
-  WatchCallbacks.registerWatchCallback(watchId, (): void => {
-    // TODO
-    // console.log('channel changed')
-  })
-  // @ts-ignore
-  await FileSystemWorker.invoke(/* OutputChannel.open */ 'FileSystem.watchFile', watchId, /* path */ uri)
+  try {
+    const watchId = Math.random()
+    WatchCallbacks.registerWatchCallback(watchId, (): void => {
+      // TODO
+      // console.log('channel changed')
+    })
+    // @ts-ignore
+    await FileSystemWorker.invoke(/* OutputChannel.open */ 'FileSystem.watchFile', watchId, /* path */ uri)
+  } catch {
+    // ignore
+  }
 }
