8c3e7750ab07f2a29db777b73dcc9eb0cca44bff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 10:45:12 +0200

    feature: update output e2e test (#108)

--- packages/e2e/src/output.open.ts
+++ packages/e2e/src/output.open.ts
@@ -5,13 +5,19 @@ export const name = 'output.open'
 export const skip = 1
 
 // TODO add page object
-export const test: Test = async ({ FileSystem, Panel, Extension }) => {
+export const test: Test = async ({ Command, FileSystem, Panel, Extension, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/test.txt`, 'div')
   const extensionUri = import.meta.resolve('../fixtures/sample.output-channel-basic').toString()
   await Extension.addWebExtension(extensionUri)
   await Panel.open('Output')
+  await Command.execute('Panel.selectIndex', 1)
 
   // act
+  await Command.execute('Output.selectChannel', 'xyz')
+
+  // assert
+  const text = Locator('.Output .Message')
+  await expect(text).toHaveText('abc')
 }
