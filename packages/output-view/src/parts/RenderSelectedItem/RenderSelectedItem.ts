adc5a4acff5d8b534f060516cb37b2be3a253667
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-14 11:06:15 +0200

    feature: add selected item diff (#110)

---
+++ packages/output-view/src/parts/RenderSelectedItem/RenderSelectedItem.ts
@@ -0,0 +1,7 @@
+import type { OutputState } from '../OutputState/OutputState.ts'
+import type { ViewletCommand } from '../ViewletCommand/ViewletCommand.ts'
+
+export const renderSelectedItem = (oldState: OutputState, newState: OutputState): ViewletCommand => {
+  // TODO
+  return ['']
+}
