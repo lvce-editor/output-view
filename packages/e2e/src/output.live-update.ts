595b4beae04f043b3401a82778484cc792390542
Le Vivilet <levivlet@pm.me>
2025-08-15 10:23:57 +0200

    feature: add live update e2e test

---
+++ packages/e2e/src/output.live-update.ts
@@ -0,0 +1,25 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'output.live-update'
+
+export const skip = 1
+
+// TODO add page object
+export const test: Test = async ({ Command, FileSystem, Panel, Extension, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.txt`, 'div')
+  const extensionUri = import.meta.resolve('../fixtures/sample.output-channel-basic').toString()
+  await Extension.addWebExtension(extensionUri)
+  await Panel.open('Output')
+  await Command.execute('Panel.selectIndex', 1)
+
+  // act
+  await Command.execute('Output.selectChannel', 'xyz')
+
+  // assert
+  const select = Locator('[name="output"]')
+  await expect(select).toHaveValue('xyz')
+  const text = Locator('.OutputContent')
+  await expect(text).toHaveText('test content')
+}
