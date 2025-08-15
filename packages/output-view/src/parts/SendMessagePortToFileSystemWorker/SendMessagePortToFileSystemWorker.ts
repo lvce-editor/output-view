92b6be8a3c21910a1543b19c5be2f4091c9ebbd1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-15 15:28:22 +0200

    feature: use rpc id (#137)

--- packages/output-view/src/parts/SendMessagePortToFileSystemWorker/SendMessagePortToFileSystemWorker.ts
+++ packages/output-view/src/parts/SendMessagePortToFileSystemWorker/SendMessagePortToFileSystemWorker.ts
@@ -1,5 +1,6 @@
+import { RpcId } from '@lvce-editor/rpc-registry'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const sendMessagePortToFileSystemWorker = async (port: any): Promise<void> => {
-  await RendererWorker.sendMessagePortToFileSystemWorker(port, 0)
+  await RendererWorker.sendMessagePortToFileSystemWorker(port, RpcId.OutputWorker)
 }
