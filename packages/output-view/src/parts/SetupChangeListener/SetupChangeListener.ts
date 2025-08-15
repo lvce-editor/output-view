44931714f07f431e1e723115364ea63cf03791fc
Le Vivilet <levivlet@pm.me>
2025-08-15 15:21:56 +0200

    feature: pass rpc id to file watcher

--- packages/output-view/src/parts/SetupChangeListener/SetupChangeListener.ts
+++ packages/output-view/src/parts/SetupChangeListener/SetupChangeListener.ts
@@ -1,15 +1,25 @@
 import { FileSystemWorker } from '@lvce-editor/rpc-registry'
+import * as OutputStates from '../OutputStates/OutputStates.ts'
 import * as WatchCallbacks from '../WatchCallbacks/WatchCallbacks.ts'
+
+const handleChange = (watchId: number): void => {
+  // console.log('channel changed', watchId)
+  const keys = OutputStates.getKeys()
+  for (const key of keys) {
+    const instance = OutputStates.get(key)
+    if (instance.newState.watchId) {
+      // TODO update this instance
+    }
+  }
+}
 
 export const setupChangeListener = async (uri: string): Promise<void> => {
   try {
     const watchId = Math.random()
-    WatchCallbacks.registerWatchCallback(watchId, (): void => {
-      // TODO
-      // console.log('channel changed')
-    })
+    const rpcId = 7001
+    WatchCallbacks.registerWatchCallback(watchId, handleChange)
     // @ts-ignore
-    await FileSystemWorker.invoke(/* OutputChannel.open */ 'FileSystem.watchFile', watchId, /* path */ uri)
+    await FileSystemWorker.invoke(/* OutputChannel.open */ 'FileSystem.watchFile', watchId, /* path */ uri, rpcId)
   } catch {
     // ignore
   }
