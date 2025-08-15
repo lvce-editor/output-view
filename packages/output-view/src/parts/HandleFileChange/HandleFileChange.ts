99a336b9f95dcae7c93cab795e66e51fa8beeaa9
Le Vivilet <levivlet@pm.me>
2025-08-15 16:13:07 +0200

    feature: call refresh function on file change

--- packages/output-view/src/parts/HandleFileChange/HandleFileChange.ts
+++ packages/output-view/src/parts/HandleFileChange/HandleFileChange.ts
@@ -1,22 +1,13 @@
-import { filterItems } from '../FilterItems/FilterItems.ts'
-import { loadLines } from '../LoadLines/LoadLines.ts'
 import * as OutputStates from '../OutputStates/OutputStates.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const handleFileChange = async (watchId: number): Promise<void> => {
   const keys = OutputStates.getKeys()
   for (const key of keys) {
     const instance = OutputStates.get(key)
     if (instance.newState.watchId === watchId) {
-      // TODO race condition
-      const { lines, error, code } = await loadLines(instance.newState.uri)
-      const filteredItems = filterItems(lines, instance.newState.filterValue)
-      OutputStates.set(instance.newState.uid, instance.oldState, {
-        ...instance.newState,
-        listItems: lines,
-        filteredItems,
-        error,
-        errorCode: code,
-      })
+      // @ts-ignore
+      await RendererWorker.invoke('Output.refresh')
     }
   }
 }
