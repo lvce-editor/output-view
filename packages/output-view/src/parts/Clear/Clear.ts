12ef9b9b07e90aed8def9f71fe8071e90b07c10d
Le Vivilet <levivlet@pm.me>
2025-08-15 22:58:32 +0200

    feature: update filtered items on clear

--- packages/output-view/src/parts/Clear/Clear.ts
+++ packages/output-view/src/parts/Clear/Clear.ts
@@ -1,9 +1,10 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
 import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.ts'
+import { filterItems } from '../FilterItems/FilterItems.ts'
 import { loadLines } from '../LoadLines/LoadLines.ts'
 
 export const clear = async (state: OutputState): Promise<OutputState> => {
-  const { selectedOption, options } = state
+  const { selectedOption, options, filterValue } = state
   // TODO make uri a property of state to make the code simpler
   const option = options.find((option) => option.id === selectedOption)
   if (!option) {
@@ -12,10 +13,12 @@ export const clear = async (state: OutputState): Promise<OutputState> => {
   const { uri } = option
   await FileSystemWorker.writeFile(uri, '')
   const { lines, code, error } = await loadLines(uri)
+  const filteredItems = filterItems(lines, filterValue)
   return {
     ...state,
     listItems: lines,
     error,
     errorCode: code,
+    filteredItems,
   }
 }
