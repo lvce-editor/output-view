91db06860f2377ff4de8962b807c969e5be2bfbd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-15 22:51:55 +0200

    feature: add clear filter e2e test (#156)

---
+++ packages/e2e/src/output.clear-filter.ts
@@ -0,0 +1,29 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'output.clear'
+
+export const skip = 1
+
+// TODO add page object
+export const test: Test = async ({ Command, FileSystem, Panel, Extension, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.txt`, 'div')
+  const extensionUri = import.meta.resolve('../fixtures/sample.output-channel-filter').toString()
+  await Extension.addWebExtension(extensionUri)
+  await Panel.open('Output')
+  await Command.execute('Panel.selectIndex', 1)
+
+  // act
+  await Command.execute('Output.selectChannel', 'xyz')
+
+  // assert
+  const select = Locator('[name="output"]')
+  await expect(select).toHaveValue('xyz')
+  const text = Locator('.OutputContent')
+  await expect(text).toHaveText('test content atest content b')
+
+  // act
+  await Command.execute('Output.handleFiterInput', 'content b')
+  await expect(text).toHaveText('test content b')
+}
