3e4de3052248c854b3694f5bcc2a03d68af70017
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-15 11:25:44 +0200

    feature: setup change listener again when channel changes (#134)

--- packages/output-view/src/parts/SelectChannel/SelectChannel.ts
+++ packages/output-view/src/parts/SelectChannel/SelectChannel.ts
@@ -1,6 +1,7 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
 import { filterItems } from '../FilterItems/FilterItems.ts'
 import { loadLines } from '../LoadLines/LoadLines.ts'
+import { setupChangeListener } from '../SetupChangeListener/SetupChangeListener.ts'
 
 export const selectChannel = async (state: OutputState, id: string): Promise<OutputState> => {
   const { options, filterValue } = state
@@ -10,6 +11,10 @@ export const selectChannel = async (state: OutputState, id: string): Promise<Out
   }
   // TODO potential race condtion when switching output channels fast
   const { lines, error, code } = await loadLines(matchingOption.uri)
+
+  // TODO memory leak and race condition, need to dispose file watcher of previous uri
+  await setupChangeListener(matchingOption.uri)
+
   const filteredItems = filterItems(lines, filterValue)
   return {
     ...state,
