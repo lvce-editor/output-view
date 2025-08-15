d090f549c883ad5782a4249faa92ad643fb1a2eb
Le Vivilet <levivlet@pm.me>
2025-08-15 16:05:05 +0200

    feature: use file change function

--- packages/output-view/src/parts/SetupChangeListener/SetupChangeListener.ts
+++ packages/output-view/src/parts/SetupChangeListener/SetupChangeListener.ts
@@ -1,22 +1,11 @@
 import { FileSystemWorker, RpcId } from '@lvce-editor/rpc-registry'
-import * as OutputStates from '../OutputStates/OutputStates.ts'
+import { handleFileChange } from '../HandleFileChange/HandleFileChange.ts'
 import * as WatchCallbacks from '../WatchCallbacks/WatchCallbacks.ts'
-
-const handleChange = (watchId: number): void => {
-  // console.log('channel changed', watchId)
-  const keys = OutputStates.getKeys()
-  for (const key of keys) {
-    const instance = OutputStates.get(key)
-    if (instance.newState.watchId) {
-      // TODO update this instance
-    }
-  }
-}
 
 export const setupChangeListener = async (watchId: number, uri: string): Promise<void> => {
   try {
     const rpcId = RpcId.OutputWorker
-    WatchCallbacks.registerWatchCallback(watchId, handleChange)
+    WatchCallbacks.registerWatchCallback(watchId, handleFileChange)
     // @ts-ignore
     await FileSystemWorker.invoke(/* OutputChannel.open */ 'FileSystem.watchFile', watchId, /* path */ uri, rpcId)
   } catch {
