11dbbdde112fa699fb0ef760a04d9e63e42bf604
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-15 22:44:36 +0200

    feature: add name property to filter input field (#153)

--- packages/output-view/src/parts/GetFilterVirtualDom/GetFilterVirtualDom.ts
+++ packages/output-view/src/parts/GetFilterVirtualDom/GetFilterVirtualDom.ts
@@ -1,6 +1,7 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 export const getFilterVirtualDom = (): readonly VirtualDomNode[] => {
   const placeholder = 'Filter'
@@ -16,6 +17,7 @@ export const getFilterVirtualDom = (): readonly VirtualDomNode[] => {
       childCount: 0,
       placeholder,
       onInput: DomEventListenerFunctions.HandleFilterInput,
+      name: InputName.Filter,
     },
   ]
 }
