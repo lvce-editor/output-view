5bec357ec0db2dd93a55f7b399c3ac3a904c466a
Le Vivilet <levivlet@pm.me>
2025-08-15 22:39:45 +0200

    feature: set input source for filter input

--- packages/output-view/src/parts/HandleFilterInput/HandleFilterInput.ts
+++ packages/output-view/src/parts/HandleFilterInput/HandleFilterInput.ts
@@ -1,12 +1,13 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
 import { filterItems } from '../FilterItems/FilterItems.ts'
 
-export const handleFilterInput = async (state: OutputState, value: string): Promise<OutputState> => {
+export const handleFilterInput = async (state: OutputState, value: string, inputSource: number): Promise<OutputState> => {
   const { listItems } = state
   const filteredItems = filterItems(listItems, value)
   return {
     ...state,
     filterValue: value,
     filteredItems: filteredItems,
+    inputSource,
   }
 }
