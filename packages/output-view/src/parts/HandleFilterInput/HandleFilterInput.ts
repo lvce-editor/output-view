f14a928766c41cdf77f27a47cf0f0e7ddd340369
Le Vivilet <levivlet@pm.me>
2025-08-15 22:42:45 +0200

    tsc

--- packages/output-view/src/parts/HandleFilterInput/HandleFilterInput.ts
+++ packages/output-view/src/parts/HandleFilterInput/HandleFilterInput.ts
@@ -1,7 +1,8 @@
 import type { OutputState } from '../OutputState/OutputState.ts'
 import { filterItems } from '../FilterItems/FilterItems.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
-export const handleFilterInput = async (state: OutputState, value: string, inputSource: number): Promise<OutputState> => {
+export const handleFilterInput = async (state: OutputState, value: string, inputSource: number = InputSource.User): Promise<OutputState> => {
   const { listItems } = state
   const filteredItems = filterItems(listItems, value)
   return {
