2b380483a52a53aaf09b16e1ddcefa98112db161
Le Vivilet <levivlet@pm.me>
2025-08-15 16:14:46 +0200

    fix: refresh

--- packages/output-view/src/parts/Refresh/Refresh.ts
+++ packages/output-view/src/parts/Refresh/Refresh.ts
@@ -3,8 +3,12 @@ import { filterItems } from '../FilterItems/FilterItems.ts'
 import { loadLines } from '../LoadLines/LoadLines.ts'
 
 export const refresh = async (state: OutputState): Promise<OutputState> => {
-  const { uri, filterValue } = state
-  const { lines, error, code } = await loadLines(uri)
+  const { filterValue, options, selectedOption } = state
+  const matchingOption = options.find((option) => option.id === selectedOption)
+  if (!matchingOption) {
+    return state
+  }
+  const { lines, error, code } = await loadLines(matchingOption.uri)
   const filteredItems = filterItems(lines, filterValue)
   return {
     ...state,
