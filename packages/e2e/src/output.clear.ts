86a0feafec40108db22cd653142053f90c6836f1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-15 22:56:03 +0200

    feature: improve clear e2e test (#158)

--- packages/e2e/src/output.clear.ts
+++ packages/e2e/src/output.clear.ts
@@ -9,7 +9,7 @@ export const test: Test = async ({ Command, FileSystem, Panel, Extension, Locato
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/test.txt`, 'div')
-  const extensionUri = import.meta.resolve('../fixtures/sample.output-channel-filter').toString()
+  const extensionUri = import.meta.resolve('../fixtures/sample.output-channel-basic').toString()
   await Extension.addWebExtension(extensionUri)
   await Panel.open('Output')
   await Command.execute('Panel.selectIndex', 1)
@@ -21,9 +21,11 @@ export const test: Test = async ({ Command, FileSystem, Panel, Extension, Locato
   const select = Locator('[name="output"]')
   await expect(select).toHaveValue('xyz')
   const text = Locator('.OutputContent')
-  await expect(text).toHaveText('test content atest content b')
+  await expect(text).toHaveText('test content')
 
   // act
-  await Command.execute('Output.handleFiterInput', 'content b')
-  await expect(text).toHaveText('test content b')
+  await Command.execute('Output.clear')
+
+  // assert
+  await expect(text).toHaveText('')
 }
