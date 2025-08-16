8cc304262c1764b1cf10fb027ffa41d8be9592c8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 09:48:02 +0200

    feature: use classnames enum (#172)

--- packages/output-view/src/parts/GetLogFileNotFoundDom/GetLogFileNotFoundDom.ts
+++ packages/output-view/src/parts/GetLogFileNotFoundDom/GetLogFileNotFoundDom.ts
@@ -1,12 +1,13 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as OutputStrings from '../OutputStrings/OutputStrings.ts'
 
 export const getLogFileNotFoundDom = (): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
-      className: 'Message',
+      className: ClassNames.Message,
       tabIndex: 0,
       childCount: 1,
     },
