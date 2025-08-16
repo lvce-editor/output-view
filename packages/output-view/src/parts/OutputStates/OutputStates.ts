41e2ea27b33d1373571a82f2c61e696efcb94b79
Le Vivilet <levivlet@pm.me>
2025-08-16 17:30:18 +0200

    feature: simplify command ids

--- packages/output-view/src/parts/OutputStates/OutputStates.ts
+++ packages/output-view/src/parts/OutputStates/OutputStates.ts
@@ -1,4 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor//viewlet-registry'
 import type { OutputState } from '../OutputState/OutputState.ts'
 
-export const { get, set, wrapCommand, wrapGetter, getKeys } = ViewletRegistry.create<OutputState>()
+export const { get, set, wrapCommand, wrapGetter, getKeys, getCommandIds } = ViewletRegistry.create<OutputState>()
