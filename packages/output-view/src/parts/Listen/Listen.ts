c068adbe0fcf03312a3de2858a5b2dd914f3af47
Le Vivilet <levivlet@pm.me>
2025-08-16 19:21:36 +0200

    fix: register commands

--- packages/output-view/src/parts/Listen/Listen.ts
+++ packages/output-view/src/parts/Listen/Listen.ts
@@ -1,9 +1,11 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as CommandMapRef from '../CommandMapRef/CommandMapRef.ts'
+import { registerCommands } from '../OutputStates/OutputStates.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
+  registerCommands(CommandMap.commandMap)
   Object.assign(CommandMapRef.commandMapRef, CommandMap.commandMap)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMapRef.commandMapRef,
