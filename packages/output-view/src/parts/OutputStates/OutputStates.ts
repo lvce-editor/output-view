d4b8da4760f558a1cdaa8cf9f3d6de6ea09b13bd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 19:22:33 +0200

    fix: register commands (#214)

--- packages/output-view/src/parts/OutputStates/OutputStates.ts
+++ packages/output-view/src/parts/OutputStates/OutputStates.ts
@@ -1,4 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor//viewlet-registry'
 import type { OutputState } from '../OutputState/OutputState.ts'
 
-export const { get, set, wrapCommand, wrapGetter, getKeys, getCommandIds } = ViewletRegistry.create<OutputState>()
+export const { get, set, wrapCommand, wrapGetter, getKeys, getCommandIds, registerCommands, dispose } = ViewletRegistry.create<OutputState>()
