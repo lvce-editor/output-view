dc6f4ea95fcd6a5beb8c1d588000156b79f3b9ae
Le Vivilet <levivlet@pm.me>
2025-08-16 10:22:55 +0200

    feature: add string tests

--- packages/output-view/test/OutputStrings.test.ts
+++ packages/output-view/test/OutputStrings.test.ts
@@ -8,3 +8,14 @@ test('OutputStrings - returns localized strings', () => {
   expect(OutputStrings.openLogFile()).toBe('open output log file')
   expect(OutputStrings.logFileNotFound()).toBe('The log file was not found.')
 })
+
+test('OutputStrings - returns all localized strings', () => {
+  expect(OutputStrings.output()).toBe('output')
+  expect(OutputStrings.clearOutput()).toBe('clear output')
+  expect(OutputStrings.turnOffAutoScroll()).toBe('Turn auto scrolling off')
+  expect(OutputStrings.settings()).toBe('Settings')
+  expect(OutputStrings.openLogFile()).toBe('open output log file')
+  expect(OutputStrings.setLogLevel()).toBe('Set Log Level')
+  expect(OutputStrings.logFileNotFound()).toBe('The log file was not found.')
+  expect(OutputStrings.filter()).toBe('Filter')
+})
