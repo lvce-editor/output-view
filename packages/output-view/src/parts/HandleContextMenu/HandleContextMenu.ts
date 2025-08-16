6941cc8c9c93c4a56ca269b65a7eb8360a40fbc8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 08:27:18 +0200

    feature: add context menu function (#160)

---
+++ packages/output-view/src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -0,0 +1,6 @@
+import type { OutputState } from '../OutputState/OutputState.ts'
+
+export const handleContextMenu = async (state: OutputState, x: number, y: number, button: number): Promise<OutputState> => {
+  // TODO open context menu
+  return state
+}
