8b81bf7c8363e12f9326612b84d8441e13262fb8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 08:31:46 +0200

    feature: add saveOutputAs (#161)

--- packages/output-view/src/parts/CommandMap/CommandMap.ts
+++ packages/output-view/src/parts/CommandMap/CommandMap.ts
@@ -22,14 +22,15 @@ import * as Render2 from '../Render2/Render2.ts'
 import { renderActions } from '../RenderActions/RenderActions.ts'
 import { renderEventListeners } from '../RenderEventListeners/RenderEventListeners.ts'
 import * as Resize from '../Resize/Resize.ts'
+import { saveOutputAs } from '../SaveOutputAs/SaveOutputAs.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
 import { selectChannel } from '../SelectChannel/SelectChannel.ts'
 import { setOutputChannel } from '../SetOutputChannel/SetOutputChannel.ts'
 import * as WatchCallback from '../WatchCallbacks/WatchCallbacks.ts'
 
 export const commandMap = {
+  'Output.clear': WrapCommand.wrapCommand(Clear.clear),
   'Output.closeFindWidget': WrapCommand.wrapCommand(closeFindWidget),
-  'Output.handleContextMenu': WrapCommand.wrapCommand(handleContextMenu),
   'Output.create': Create.create,
   'Output.diff2': Diff2.diff2,
   'Output.executeWatchCallback': WatchCallback.executeWatchCallBack,
@@ -38,21 +39,22 @@ export const commandMap = {
   'Output.getCommandIds': GetCommandIds.getCommandIds,
   'Output.getKeyBindings': GetKeyBindings.getKeyBindings,
   'Output.handleButtonClick': WrapCommand.wrapCommand(handleButtonClick),
+  'Output.handleContextMenu': WrapCommand.wrapCommand(handleContextMenu),
   'Output.handleData': WrapCommand.wrapCommand(handleData),
   'Output.handleError': WrapCommand.wrapCommand(handleError),
-  'Output.selectChannel': WrapCommand.wrapCommand(selectChannel),
-  'Output.clear': WrapCommand.wrapCommand(Clear.clear),
   'Output.handleFilterInput': WrapCommand.wrapCommand(handleFilterInput),
   'Output.handleSelect': WrapCommand.wrapCommand(handleSelect),
   'Output.initialize': Initialize.initialize,
   'Output.loadContent2': WrapCommand.wrapCommand(loadContent),
   'Output.openFindWidget': WrapCommand.wrapCommand(openFindWidget),
+  'Output.refresh': WrapCommand.wrapCommand(refresh),
   'Output.render2': Render2.render2,
   'Output.renderActions': WrapCommand.wrapGetter(renderActions),
   'Output.renderEventListeners': renderEventListeners,
   'Output.resize': Resize.resize,
+  'Output.saveOutputAs': WrapCommand.wrapCommand(saveOutputAs),
   'Output.saveState': SaveState.saveState,
+  'Output.selectChannel': WrapCommand.wrapCommand(selectChannel),
   'Output.setOutputChannel': WrapCommand.wrapCommand(setOutputChannel),
   'Output.terminate': ViewletRegistry.terminate,
-  'Output.refresh': WrapCommand.wrapCommand(refresh),
 }
---
+++ packages/output-view/src/parts/SaveOutputAs/SaveOutputAs.ts
@@ -0,0 +1,8 @@
+import type { OutputState } from '../OutputState/OutputState.ts'
+
+export const saveOutputAs = async (state: OutputState): Promise<OutputState> => {
+  // TODO show open file picker
+  // TODO write to file
+  // TODO if error occurs, show error dialog
+  return state
+}
