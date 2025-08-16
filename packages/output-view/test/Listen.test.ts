35d7c5f2d8c5fcb0bbb206cd334026202d6458d0
Le Vivilet <levivlet@pm.me>
2025-08-16 17:44:43 +0200

    lint

--- packages/output-view/test/Listen.test.ts
+++ packages/output-view/test/Listen.test.ts
@@ -1,11 +1,13 @@
-import { test } from '@jest/globals'
+import { expect, test } from '@jest/globals'
 import { mockWorkerGlobalRpc } from '@lvce-editor/rpc'
 import { listen } from '../src/parts/Listen/Listen.ts'
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
 
 test('listen', async () => {
   const { start, dispose } = mockWorkerGlobalRpc()
   const listenPromise = listen()
   start()
   await listenPromise
+  expect(RpcRegistry.get(RpcRegistry.RpcId.RendererWorker)).toBeDefined()
   dispose()
 })
