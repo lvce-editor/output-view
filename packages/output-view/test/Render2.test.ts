1f00941d4603133254373921c3ca82c1261f424f
Le Vivilet <levivlet@pm.me>
2025-08-16 17:31:26 +0200

    feature: add render test

--- packages/output-view/test/Render2.test.ts
+++ packages/output-view/test/Render2.test.ts
@@ -4,7 +4,7 @@ import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaul
 import { render2 } from '../src/parts/Render2/Render2.ts'
 
 test('should throw error when uid not found in OutputStates', () => {
-  const uid = 999999 // Use a uid that definitely doesn't exist
+  const uid = 999_999 // Use a uid that definitely doesn't exist
   const diffResult = [1, 2, 3]
 
   // The function should throw when trying to get a non-existent uid
@@ -12,7 +12,7 @@ test('should throw error when uid not found in OutputStates', () => {
 })
 
 test('should throw error with invalid diff result', () => {
-  const uid = 888888 // Use another uid that doesn't exist
+  const uid = 888_888 // Use another uid that doesn't exist
   const diffResult = [999] // Use an invalid diff type
 
   // The function should throw when trying to get a non-existent uid
