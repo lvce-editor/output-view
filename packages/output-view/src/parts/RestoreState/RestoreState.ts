67b53c75b370ce06881f6bf6cbfcb84d487d9a43
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 10:05:51 +0200

    feature: restore scrollLockEnabled (#179)

--- packages/output-view/src/parts/RestoreState/RestoreState.ts
+++ packages/output-view/src/parts/RestoreState/RestoreState.ts
@@ -14,11 +14,20 @@ const getSavedSelectedOption = (savedState: any): string => {
   return ''
 }
 
+const getSavedScrollLockEnabled = (savedState: any): boolean => {
+  if (savedState && typeof savedState.scrollLockEnabled === 'boolean') {
+    return savedState.scrollLockEnabled
+  }
+  return false
+}
+
 export const restoreState = (savedState: unknown): RestoredState => {
   const filterValue = getSavedFilterValue(savedState)
   const selectedOption = getSavedSelectedOption(savedState)
+  const scrollLockEnabled = getSavedScrollLockEnabled(savedState)
   return {
     filterValue,
     selectedOption,
+    scrollLockEnabled,
   }
 }
