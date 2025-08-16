cebbe02a8cc568422c91dbf0682646db44b8e271
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-16 14:13:09 +0200

    feature: add file system worker test (#193)

---
+++ packages/output-view/test/InitializeFileSystemWorker.test.ts
@@ -0,0 +1,28 @@
+import { expect, jest, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as RpcRegistry from '@lvce-editor/rpc-registry'
+import { initializeFileSystemWorker } from '../src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
+
+test('initializeFileSystemWorker registers FileSystemWorker RPC', async () => {
+  const invokeAndTransfer = jest.fn()
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      throw new Error(`unexpected method ${method}`)
+    },
+    invokeAndTransfer,
+  })
+  RendererWorker.set(mockRpc)
+  await initializeFileSystemWorker()
+  expect(invokeAndTransfer).toHaveBeenCalledTimes(1)
+  expect(invokeAndTransfer).toHaveBeenCalledWith(
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToFileSystemWorker',
+    expect.any(MessagePort),
+    'FileSystem.handleMessagePort',
+    RpcRegistry.RpcId.OutputWorker,
+  )
+  const rpc = RpcRegistry.get(RpcRegistry.RpcId.FileSystemWorker)
+  expect(rpc).toBeDefined()
+  await rpc.dispose()
+})
