6682db4155881da0bb0ccf28f4cfeabd2b7ddf78
Le Vivilet <levivlet@pm.me>
2025-08-16 17:49:07 +0200

    lint

--- packages/output-view/test/Listen.test.ts
+++ packages/output-view/test/Listen.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { mockWorkerGlobalRpc } from '@lvce-editor/rpc'
-import { listen } from '../src/parts/Listen/Listen.ts'
 import * as RpcRegistry from '@lvce-editor/rpc-registry'
+import { listen } from '../src/parts/Listen/Listen.ts'
 
 test('listen', async () => {
   const { start, dispose } = mockWorkerGlobalRpc()
