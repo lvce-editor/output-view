b42da0fb73f2d6c22ed6d23e1b8c9e7655f4ab72
Le Vivilet <levivlet@pm.me>
2025-08-16 14:08:46 +0200

    test

--- packages/output-view/test/InitializeFileSystemWorker.test.ts
+++ packages/output-view/test/InitializeFileSystemWorker.test.ts
@@ -1,22 +1,23 @@
-import { test, expect } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
-import * as FileSystemWorker from '../src/parts/FileSystemWorker/FileSystemWorker.ts'
 import * as RpcRegistry from '@lvce-editor/rpc-registry'
 import { initializeFileSystemWorker } from '../src/parts/InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
 test('initializeFileSystemWorker registers FileSystemWorker RPC', async () => {
   // Create a mock RPC
+  const invokeAndTransfer = jest.fn()
   const mockRpc = MockRpc.create({
     commandMap: {},
     invoke: (method: string) => {
-      if (method === 'FileSystem.readDirWithFileTypes') {
-        return Promise.resolve([])
-      }
       throw new Error(`unexpected method ${method}`)
     },
+    invokeAndTransfer,
   })
-  FileSystemWorker.set(mockRpc)
+  RendererWorker.set(mockRpc)
   await initializeFileSystemWorker()
+  expect(invokeAndTransfer).toHaveBeenCalledTimes(1)
+  expect(invokeAndTransfer).toHaveBeenCalledWith('FileSystemWorker', 'initialize', undefined)
   const rpc = RpcRegistry.get(RpcRegistry.RpcId.FileSystemWorker)
   expect(rpc).toBeDefined()
   await rpc.dispose()
