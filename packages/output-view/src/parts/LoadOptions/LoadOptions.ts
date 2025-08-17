a5a79a0ec040c6411e7782600f077a8fce33ecd8
Le Vivilet <levivlet@pm.me>
2025-08-17 08:48:39 +0200

    try to fix windows

--- packages/output-view/src/parts/LoadOptions/LoadOptions.ts
+++ packages/output-view/src/parts/LoadOptions/LoadOptions.ts
@@ -3,7 +3,7 @@ import { getExtensionOptions } from '../GetExtensionOptions/GetExtensionOptions.
 import * as InputName from '../InputName/InputName.ts'
 import * as PlatformType from '../PlatformType/PlatformType.ts'
 
-export const loadOptions = async (platform: number): Promise<readonly Option[]> => {
+export const loadOptions = async (platform: number, logsFolderPath: string): Promise<readonly Option[]> => {
   const extensionOptions = await getExtensionOptions()
   if (platform === PlatformType.Web) {
     return extensionOptions
@@ -12,12 +12,12 @@ export const loadOptions = async (platform: number): Promise<readonly Option[]> 
     {
       id: InputName.MainProcess,
       label: 'Main Process',
-      uri: 'file:///tmp/log-main-process.txt',
+      uri: `${logsFolderPath}/log-main-process.txt`,
     },
     {
       id: InputName.SharedProcess,
       label: 'Shared Process',
-      uri: 'file:///tmp/log-shared-process.txt',
+      uri: `${logsFolderPath}/log-shared-process.txt`,
     },
     ...extensionOptions,
   ]
