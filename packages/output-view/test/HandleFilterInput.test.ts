fdfe114439155a18efef86db3db265e1deed0a6b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 08:59:27 +0200

    Discard changes to packages/output-view/test/HandleFilterInput.test.ts

--- packages/output-view/test/HandleFilterInput.test.ts
+++ packages/output-view/test/HandleFilterInput.test.ts
@@ -2,22 +2,10 @@ import { test, expect } from '@jest/globals'
 import type { OutputState } from '../src/parts/OutputState/OutputState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { handleFilterInput } from '../src/parts/HandleFilterInput/HandleFilterInput.ts'
-import * as LinePartType from '../src/parts/LinePartType/LinePartType.ts'
 
 test('handleFilterInput - updates filter and filteredItems', async () => {
-  const state: OutputState = {
-    ...createDefaultState(),
-    listItems: [
-      [{ type: LinePartType.Text, value: 'apple' }],
-      [{ type: LinePartType.Text, value: 'banana' }],
-      [{ type: LinePartType.Text, value: 'carrot' }],
-    ],
-  }
+  const state: OutputState = { ...createDefaultState(), listItems: ['apple', 'banana', 'carrot'] }
   const result = await handleFilterInput(state, 'a')
   expect(result.filterValue).toBe('a')
-  expect(result.filteredItems).toEqual([
-    [{ type: LinePartType.Text, value: 'apple' }],
-    [{ type: LinePartType.Text, value: 'banana' }],
-    [{ type: LinePartType.Text, value: 'carrot' }],
-  ])
+  expect(result.filteredItems).toEqual(['apple', 'banana', 'carrot'])
 })
