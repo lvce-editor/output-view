da3e922adbb8f617dbb65d8a40ad17d3d764c659
Le Vivilet <levivlet@pm.me>
2025-08-26 09:01:15 +0200

    rename

--- packages/output-view/src/parts/GetLinkMatch/GetLinkMatch.ts
+++ packages/output-view/src/parts/GetLinkMatch/GetLinkMatch.ts
@@ -1,6 +1,6 @@
-const urlRegex = /(?:https?:\/\/\S+|file:\/\/\S+)/
+const RE_URL = /(?:https?:\/\/\S+|file:\/\/\S+)/
 
 export const getLinkMatch = (text: string): string | null => {
-  const m = text.match(urlRegex)
+  const m = text.match(RE_URL)
   return m ? m[0] : null
 }
