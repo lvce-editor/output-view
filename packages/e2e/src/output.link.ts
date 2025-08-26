e8ebd70dce6d24d4de4a727abe94bad00de77751
Le Vivilet <levivlet@pm.me>
2025-08-26 09:13:26 +0200

    fix

--- packages/e2e/src/output.link.ts
+++ packages/e2e/src/output.link.ts
@@ -19,6 +19,6 @@ export const test: Test = async ({ Command, FileSystem, Panel, Extension, Locato
   await expect(link).toBeVisible()
   await expect(link).toHaveText('https://example.com')
   await expect(link).toHaveAttribute('target', '_blank')
-  await expect(link).toHaveAttribute('rel', 'noopener noreferer')
+  await expect(link).toHaveAttribute('rel', 'noopener noreferrer')
   await expect(link).toHaveAttribute('href', 'https://example.com')
 }
