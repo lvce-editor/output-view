b6ad15a00ffb9745f289882340c8f750a31e17c2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 08:58:17 +0200

    Discard changes to packages/output-view/src/parts/GetOutputVirtualDom/GetOutputVirtualDom.ts

--- packages/output-view/src/parts/GetOutputVirtualDom/GetOutputVirtualDom.ts
+++ packages/output-view/src/parts/GetOutputVirtualDom/GetOutputVirtualDom.ts
@@ -1,11 +1,10 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { mergeClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import type { Line } from '../Line/Line.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getContentDom } from '../GetContentDom/GetContentDom.ts'
 import { getErrorDom } from '../GetErrorDom/GetErrorDom.ts'
 
-export const getOutputVirtualDom = (lines: readonly Line[], errorCode: number, error: string): readonly VirtualDomNode[] => {
+export const getOutputVirtualDom = (lines: readonly string[], errorCode: number, error: string): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
