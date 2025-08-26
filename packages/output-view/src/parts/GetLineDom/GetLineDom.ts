5b7cbee0e71e9a782fc05ef2649f32f1126421c6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 08:58:11 +0200

    Discard changes to packages/output-view/src/parts/GetLineDom/GetLineDom.ts

--- packages/output-view/src/parts/GetLineDom/GetLineDom.ts
+++ packages/output-view/src/parts/GetLineDom/GetLineDom.ts
@@ -1,8 +1,6 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import type { LinePart } from '../LinePart/LinePart.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as LinePartType from '../LinePartType/LinePartType.ts'
 
 const parentNode: VirtualDomNode = {
   type: VirtualDomElements.Div,
@@ -10,15 +8,6 @@ const parentNode: VirtualDomNode = {
   childCount: 1,
 }
 
-export const getLineDom = (parts: readonly LinePart[]): readonly VirtualDomNode[] => {
-  const children: VirtualDomNode[] = []
-  for (const part of parts) {
-    if (part.type === LinePartType.Text) {
-      children.push(text(part.value))
-    } else {
-      children.push({ type: VirtualDomElements.A, href: part.value, childCount: 1 }, text(part.value))
-    }
-  }
-  const lineNode: VirtualDomNode = { ...parentNode, childCount: children.length }
-  return [lineNode, ...children]
+export const getLineDom = (line: string): readonly VirtualDomNode[] => {
+  return [parentNode, text(line)]
 }
