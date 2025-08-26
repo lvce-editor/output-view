fc72a6ac3d426ca81b3035251652184894d963cf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 08:58:00 +0200

    Discard changes to packages/output-view/src/parts/FilterItems/FilterItems.ts

--- packages/output-view/src/parts/FilterItems/FilterItems.ts
+++ packages/output-view/src/parts/FilterItems/FilterItems.ts
@@ -1,17 +1,6 @@
-import type { Line } from '../Line/Line.ts'
-import type { LinePart } from '../LinePart/LinePart.ts'
-
-const getTextFromParts = (parts: readonly LinePart[]): string => {
-  let result = ''
-  for (const part of parts) {
-    result += part.value
-  }
-  return result
-}
-
-export const filterItems = (items: readonly Line[], filterValue: string): readonly Line[] => {
+export const filterItems = (items: readonly string[], filterValue: string): readonly string[] => {
   if (!filterValue) {
     return items
   }
-  return items.filter((parts) => getTextFromParts(parts).includes(filterValue))
+  return items.filter((item) => item.includes(filterValue))
 }
