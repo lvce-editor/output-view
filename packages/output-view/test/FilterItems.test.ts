ebe3e7bf7d46e7fb7ca51b31dc6e713a556fdfc6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 08:59:11 +0200

    Discard changes to packages/output-view/test/FilterItems.test.ts

--- packages/output-view/test/FilterItems.test.ts
+++ packages/output-view/test/FilterItems.test.ts
@@ -1,18 +1,12 @@
 import { test, expect } from '@jest/globals'
-import type { Line } from '../src/parts/Line/Line.ts'
 import { filterItems } from '../src/parts/FilterItems/FilterItems.ts'
-import * as LinePartType from '../src/parts/LinePartType/LinePartType.ts'
 
 test('filterItems - returns original when filter empty', () => {
-  const items: readonly Line[] = [[{ type: LinePartType.Text, value: 'a' }], [{ type: LinePartType.Text, value: 'b' }]]
+  const items = ['a', 'b']
   expect(filterItems(items, '')).toEqual(items)
 })
 
 test('filterItems - filters by substring', () => {
-  const items: readonly Line[] = [
-    [{ type: LinePartType.Text, value: 'alpha' }],
-    [{ type: LinePartType.Text, value: 'beta' }],
-    [{ type: LinePartType.Text, value: 'gamma' }],
-  ]
-  expect(filterItems(items, 'a')).toEqual(items)
+  const items = ['alpha', 'beta', 'gamma']
+  expect(filterItems(items, 'a')).toEqual(['alpha', 'beta', 'gamma'])
 })
