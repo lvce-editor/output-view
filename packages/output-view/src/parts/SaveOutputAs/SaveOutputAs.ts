93dbff3bfa81dab2af36ece513c539b5ec2bc1e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 08:58:53 +0200

    Discard changes to packages/output-view/src/parts/SaveOutputAs/SaveOutputAs.ts

--- packages/output-view/src/parts/SaveOutputAs/SaveOutputAs.ts
+++ packages/output-view/src/parts/SaveOutputAs/SaveOutputAs.ts
@@ -1,8 +1,6 @@
-// no local use of Line/LinePart here; serializeLines handles typing
 import type { OutputState } from '../OutputState/OutputState.ts'
 import * as FileSystemWorker from '../FileSystemWorker/FileSystemWorker.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
-import { serializeLines } from '../SerializeLines/SerializeLines.ts'
 
 export const saveOutputAs = async (state: OutputState): Promise<OutputState> => {
   // @ts-ignore
@@ -11,7 +9,7 @@ export const saveOutputAs = async (state: OutputState): Promise<OutputState> => 
     return state
   }
   const { listItems } = state
-  const content = serializeLines(listItems)
+  const content = listItems.join('\n')
   await FileSystemWorker.writeFile(uri, content)
   // TODO if error occurs, show error dialog
   return state
