3b255b1665fc5838583a6484c2d3ee81bf6ed396
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-26 08:59:45 +0200

    Discard changes to packages/output-view/test/SaveState.test.ts

--- packages/output-view/test/SaveState.test.ts
+++ packages/output-view/test/SaveState.test.ts
@@ -3,7 +3,6 @@ import type { OutputState } from '../src/parts/OutputState/OutputState.ts'
 import type { SavedState } from '../src/parts/SavedState/SavedState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as InputSource from '../src/parts/InputSource/InputSource.ts'
-import * as LinePartType from '../src/parts/LinePartType/LinePartType.ts'
 import { saveState } from '../src/parts/SaveState/SaveState.ts'
 
 test('saveState should extract viewMode, filterValue, and collapsedUris from OutputState', () => {
@@ -56,7 +55,7 @@ test('saveState should preserve all other state properties but only return the r
     inputSource: InputSource.Script,
     minLineY: 10,
     maxLineY: 20,
-    listItems: [[{ type: LinePartType.Text, value: '1' }]],
+    listItems: ['1'],
     collapsedUris: ['file:///collapsed1.ts', 'file:///collapsed2.ts', 'file:///collapsed3.ts'],
     smallWidthBreakPoint: 800,
   }
