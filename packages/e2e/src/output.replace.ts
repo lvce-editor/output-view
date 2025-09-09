6676fe6d7bb46927112b6022a0475b926be5c619
Le Vivilet <levivilet@pm.me>
2025-09-09 14:15:17 +0200

    feature: use file scheme for all output tests to fix windows

--- packages/e2e/src/output.replace.ts
+++ packages/e2e/src/output.replace.ts
@@ -7,7 +7,7 @@ export const skip = 1
 // TODO add page object
 export const test: Test = async ({ QuickPick, Command, FileSystem, Panel, Extension, Locator, expect }) => {
   // arrange
-  const tmpDir = await FileSystem.getTmpDir()
+  const tmpDir = await FileSystem.getTmpDir({ scheme: 'file' })
   await FileSystem.writeFile(`${tmpDir}/test.txt`, 'div')
   const extensionUri = import.meta.resolve('../fixtures/sample.output-channel-replace').toString()
   await Extension.addWebExtension(extensionUri)
