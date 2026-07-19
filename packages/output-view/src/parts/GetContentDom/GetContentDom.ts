f75cf2ac35fef1a3016dd95279793b8dfd5c6342
Le Vivilet <levivilet@pm.me>
2026-07-19 16:55:34 +0200

    feature: enable recommended virtual dom lint rules (#270)

--- packages/output-view/src/parts/GetContentDom/GetContentDom.ts
+++ packages/output-view/src/parts/GetContentDom/GetContentDom.ts
@@ -1,8 +1,9 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { Line } from '../Line/Line.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getLineDom } from '../GetLineDom/GetLineDom.ts'
+import * as TabIndex from '../TabIndex/TabIndex.ts'
 
 export const getContentDom = (lines: readonly Line[], error: string): readonly VirtualDomNode[] => {
   if (error) {
@@ -12,8 +13,8 @@ export const getContentDom = (lines: readonly Line[], error: string): readonly V
     {
       childCount: lines.length,
       className: ClassNames.OutputContent,
-      role: 'log',
-      tabIndex: 0,
+      role: AriaRoles.Log,
+      tabIndex: TabIndex.Focusable,
       type: VirtualDomElements.Div,
     },
     ...lines.flatMap(getLineDom),
--- packages/output-view/src/parts/GetLogFileNotFoundDom/GetLogFileNotFoundDom.ts
+++ packages/output-view/src/parts/GetLogFileNotFoundDom/GetLogFileNotFoundDom.ts
@@ -2,13 +2,14 @@ import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as OutputStrings from '../OutputStrings/OutputStrings.ts'
+import * as TabIndex from '../TabIndex/TabIndex.ts'
 
 export const getLogFileNotFoundDom = (): readonly VirtualDomNode[] => {
   return [
     {
       childCount: 1,
       className: ClassNames.Message,
-      tabIndex: 0,
+      tabIndex: TabIndex.Focusable,
       type: VirtualDomElements.Div,
     },
     text(OutputStrings.logFileNotFound()),
