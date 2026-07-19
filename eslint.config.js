2337c86a1667e845cc0825f232d705782e3c29a5
Le Vivilet <levivilet@pm.me>
2026-07-19 14:53:26 +0000

    feature: enable recommended virtual dom lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,19 @@ import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
+  ...config.recommendedVirtualDom,
   ...actions.default,
   {
     rules: {
       '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+    },
+  },
+  {
+    files: ['packages/output-view/test/**/*.ts'],
+    rules: {
+      'virtual-dom/prefer-constants': 'off',
+      'virtual-dom/prefer-merge-class-names': 'off',
+      'virtual-dom/prefer-state-destructuring': 'off',
     },
   },
 ]
--- packages/output-view/src/parts/GetErrorDom/GetErrorDom.ts
+++ packages/output-view/src/parts/GetErrorDom/GetErrorDom.ts
@@ -3,6 +3,7 @@ import { text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as ErrorCode from '../ErrorCode/ErrorCode.ts'
 import { getLogFileNotFoundDom } from '../GetLogFileNotFoundDom/GetLogFileNotFoundDom.ts'
+import * as TabIndex from '../TabIndex/TabIndex.ts'
 
 export const getErrorDom = (errorCode: number, error: string): readonly VirtualDomNode[] => {
   if (!error) {
@@ -15,7 +16,7 @@ export const getErrorDom = (errorCode: number, error: string): readonly VirtualD
     {
       childCount: 1,
       className: ClassNames.Error,
-      tabIndex: 0,
+      tabIndex: TabIndex.Focusable,
       type: VirtualDomElements.Div,
     },
     text(error),
