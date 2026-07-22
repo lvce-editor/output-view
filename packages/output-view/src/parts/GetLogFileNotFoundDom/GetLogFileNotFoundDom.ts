c951d5604c4b08a5b54a4d06fff8d349e34224ff
Le Vivilet <levivilet@pm.me>
2026-07-22 06:00:16 +0200

    chore: update eslint and eslint config to latest (#271)

--- packages/output-view/src/parts/GetLogFileNotFoundDom/GetLogFileNotFoundDom.ts
+++ packages/output-view/src/parts/GetLogFileNotFoundDom/GetLogFileNotFoundDom.ts
@@ -4,14 +4,13 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as OutputStrings from '../OutputStrings/OutputStrings.ts'
 import * as TabIndex from '../TabIndex/TabIndex.ts'
 
+const messageNode: VirtualDomNode = {
+  childCount: 1,
+  className: ClassNames.Message,
+  tabIndex: TabIndex.Focusable,
+  type: VirtualDomElements.Div,
+}
+
 export const getLogFileNotFoundDom = (): readonly VirtualDomNode[] => {
-  return [
-    {
-      childCount: 1,
-      className: ClassNames.Message,
-      tabIndex: TabIndex.Focusable,
-      type: VirtualDomElements.Div,
-    },
-    text(OutputStrings.logFileNotFound()),
-  ]
+  return [messageNode, text(OutputStrings.logFileNotFound())]
 }
