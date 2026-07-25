b58116cf9e0092e6e7b21da8dcb2d7c98ed46a22
Le Vivilet <levivilet@pm.me>
2026-07-25 15:00:27 +0200

    feature: migrate to npm workspaces (#272)
    * feature: migrate to npm workspaces
    
    * fix: resolve workspace-local jest

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -9,4 +9,4 @@ export const instantiationsPath = join(root, 'packages', 'output-view')
 
 export const workerPath = join(root, '.tmp/dist/dist/outputViewWorkerMain.js')
 
-export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+export const playwrightPath = new URL('../../../node_modules/playwright/index.mjs', import.meta.url).toString()
